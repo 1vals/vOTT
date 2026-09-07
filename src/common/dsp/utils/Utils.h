79c3a3f7c4126106c18b979bb4606a56fab7acb6
1vals <1valsworld@gmail.com>
2026-09-07 02:56:52 -0400

    create Utils.h
    defined 1 new overload for calculateConstant() & copied Envelope's overload

---
+++ src/common/dsp/utils/Utils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cmath>
+
+namespace vOTT::Utils {
+
+// this overload merges the calculation of the exponential factor & the constant
+[[nodiscard]] inline float calculateConstant(float ms, int sampleRate) noexcept {
+    return std::exp(1000.f / (ms * (float)sampleRate));
+}
+
+// this overload takes a pre-calculated exponential factor
+// if the time in ms is less than 0.001, it simply returns zero
+[[nodiscard]] inline float calculateConstant(float ms, float expFactor) noexcept {
+    return ms < static_cast<float>(1.0e-3) ? 0
+                                           : (std::exp(expFactor / ms));
+}
+}
