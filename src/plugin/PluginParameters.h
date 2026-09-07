d7a9642b83a40b3938589f121f74cc96abbbe5e3
1vals <1valsworld@gmail.com>
2026-09-07 04:58:20 -0400

    add expander params to apvts

--- src/plugin/PluginParameters.h
+++ src/plugin/PluginParameters.h
@@ -44,6 +44,24 @@ private:
                                                             NormalisableRange<float>(1.f, 100.f, 0.5f, 0.5),
                                                             4));
 
+        // parameters for the first expander
+        layout.add(std::make_unique<AudioParameterFloat>(ParameterID("Low Exp Threshold", 1),
+                                                          "Low Expander Threshold",
+                                                          NormalisableRange<float>(-60.f, 12.f, 0.5f),
+                                                          0));
+        layout.add(std::make_unique<AudioParameterFloat>(ParameterID("Low Exp Attack", 1),
+                                                            "Low Expander Attack",
+                                                            atkRlsRange,
+                                                            50));
+        layout.add(std::make_unique<AudioParameterFloat>(ParameterID("Low Exp Release", 1),
+                                                            "Low Expander Release",
+                                                            atkRlsRange,
+                                                            250));
+        layout.add(std::make_unique<AudioParameterFloat>(ParameterID("Low Exp Ratio", 1),
+                                                            "Low Expander Ratio",
+                                                            NormalisableRange<float>(1.f, 100.f, 0.5f, 0.5),
+                                                            4));
+
         return layout;
 
     }
