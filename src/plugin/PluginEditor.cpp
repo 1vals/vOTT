13fcc4091c6f0913acef426c0c3ff30cc29f6d27
1vals <1valsworld@gmail.com>
2026-09-11 23:45:27 -0400

    add temporary slider component

--- src/plugin/PluginEditor.cpp
+++ src/plugin/PluginEditor.cpp
@@ -10,6 +10,11 @@ OTTAudioProcessorEditor::OTTAudioProcessorEditor (OTTAudioProcessor& p)
 
     inspector.setVisible(true);
     inspector.toggle(true);
+
+    slider1.addAndMakeVisible(*this);
+    slider2.addAndMakeVisible(*this);
+    slider3.addAndMakeVisible(*this);
+    slider4.addAndMakeVisible(*this);
 }
 
 OTTAudioProcessorEditor::~OTTAudioProcessorEditor()
@@ -24,5 +29,11 @@ void OTTAudioProcessorEditor::paint (juce::Graphics& g)
 
 void OTTAudioProcessorEditor::resized()
 {
+    auto bounds = getLocalBounds();
+    int height = bounds.getHeight(), width = bounds.getWidth();
 
+    slider1.setBounds(knobset2Bounds.removeFromLeft(bounds.getWidth() / 4));
+    slider2.setBounds(knobset2Bounds.removeFromLeft(bounds.getWidth() / 3));
+    slider3.setBounds(knobset2Bounds.removeFromLeft(bounds.getWidth() / 2));
+    slider4.setBounds(knobset2Bounds);
 }
--- src/plugin/PluginEditor.h
+++ src/plugin/PluginEditor.h
@@ -3,6 +3,24 @@
 #include "PluginProcessor.h"
 #include "melatonin_inspector/melatonin_inspector.h"
 
+/** UI ROADMAP
+ *  1. implement desired wireframe
+ *  2. implement dummy sliders & knobs
+ *  3. implement band selection
+ *  4. attach knobs dynamically to selected band
+ *  5. create menu with input/output gain, and xover frequencies
+ *
+ *  LATER
+ *  create meters for the bands
+ *  design custom assets
+ */
+
+// TEMPORARY
+struct CustomRotarySlider : juce::Slider {
+    CustomRotarySlider() : Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                        juce::Slider::TextEntryBoxPosition::NoTextBox) { }
+};
+
 //==============================================================================
 class OTTAudioProcessorEditor final : public juce::AudioProcessorEditor
 {
@@ -15,10 +33,10 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     OTTAudioProcessor& processorRef;
     melatonin::Inspector inspector { *this };
+
+    CustomRotarySlider slider1, slider2, slider3, slider4;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OTTAudioProcessorEditor)
 };
